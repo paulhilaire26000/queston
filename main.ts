a25bdefb2e4c6cce87eadc8b0ba3d6694768d07a
paulhilaire26000 <124454355+paulhilaire26000@users.noreply.github.com>
2024-03-29 21:35:38 +0100

    vertion final qui marche

--- main.ts
+++ main.ts
@@ -1,23 +1,76 @@
 input.onButtonPressed(Button.A, function () {
-    reponse = 1
+    if (reponse == 0) {
+        reponse = 1
+    }
+    if (reponse == 3) {
+        reponse = 4
+    }
+    if (reponse == 6) {
+        reponse = 7
+    }
 })
 input.onButtonPressed(Button.B, function () {
-    reponse = 2
+    if (reponse == 0) {
+        reponse = 2
+    }
+    if (reponse == 3) {
+        reponse = 5
+    }
+    if (reponse == 3) {
+        reponse = 8
+    }
 })
 let reponse = 0
 reponse = 0
 basic.showString("quelle est la meilleur application a netflix b youtube")
+let score = 0
 basic.forever(function () {
     if (reponse == 1) {
         basic.clearScreen()
         basic.showString("faux")
         basic.clearScreen()
-        reponse = 0
+        score = score - 1
+        reponse = 3
+        basic.showString("quelle est le meilleur livre a miss perigrinne et les enfant particulier b autre monde")
     }
     if (reponse == 2) {
         basic.clearScreen()
         basic.showString("vrai")
         basic.clearScreen()
-        reponse = 0
+        score = score + 1
+        reponse = 3
+        basic.showString("quelle est le meilleur livre a miss perigrine et les enfant particulier b autre monde")
+    }
+    if (reponse == 4) {
+        basic.clearScreen()
+        basic.showString("vrai")
+        basic.clearScreen()
+        score = score + 1
+        reponse = 6
+        basic.showString("quelle est la meilleure personnage de miss perigrine et les enfant particulier a noor pradeche b emma bloume")
+    }
+    if (reponse == 5) {
+        basic.clearScreen()
+        basic.showString("faux")
+        basic.clearScreen()
+        score = score - 1
+        reponse = 6
+        basic.showString("quelle est la meilleure personnage de miss perigrine et les enfant particulier a noor pradeche b emma bloume")
+    }
+    if (reponse == 7) {
+        basic.clearScreen()
+        basic.showString("vrai")
+        basic.clearScreen()
+        score = score + 1
+        basic.showNumber(score)
+        score = 0
+    }
+    if (reponse == 8) {
+        basic.clearScreen()
+        basic.showString("faux")
+        basic.clearScreen()
+        score = score - 1
+        basic.showNumber(score)
+        score = 0
     }
 })
